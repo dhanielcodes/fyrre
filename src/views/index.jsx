7bf495f29e22943d822ed7f69fe17088251a5b0a
Adekoya Daniel <adekoyadaniel53@gmail.com>
2022-09-09 20:08:05 +0100

    chore: first commit

---
+++ src/views/index.jsx
@@ -0,0 +1,7 @@
+import Header from "./header"
+import Works from "./works"
+import Experience from "./experiences"
+import Authors from "./authors"
+import Contact from "./contact"
+
+export { Header, Works, Experience, Authors, Contact}
