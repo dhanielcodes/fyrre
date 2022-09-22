233b96e941b54bcfd86c4b8798b97c91e8a46e5d
Adekoya Daniel <adekoyadaniel53@gmail.com>
2022-09-22 09:54:19 +0100

    chore: update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import { Authors, Home, Magazine, Podcasts } from "./pages"
 import { useState, useRef, useEffect } from 'react'
 import {gsap} from 'gsap'
 import ScrollTrigger from "gsap/ScrollTrigger";
+import Cursor from './components/cursor';
 //import LocomotiveScroll from 'locomotive-scroll';
+
 function App() {
   const location = useLocation()
   gsap.registerPlugin(ScrollTrigger)
@@ -121,25 +123,10 @@ function App() {
   
   }
   
-
-  
-  
-  
-  
   
   return (
     <div className="App">
-              {
-                loader ? <div className="loader">
-                  <svg width="328" height="328" viewBox="0 0 328 328" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M304 164C304 182.385 300.379 200.59 293.343 217.576C286.307 234.561 275.995 249.995 262.995 262.995C249.995 275.995 234.561 286.307 217.576 293.343C200.59 300.379 182.385 304 164 304C145.615 304 127.41 300.379 110.424 293.343C93.4387 286.307 78.0052 275.995 65.005 262.995C52.0048 249.995 41.6925 234.561 34.6569 217.576C27.6212 200.59 24 182.385 24 164L69.3207 164C69.3207 176.433 71.7696 188.745 76.5277 200.232C81.2858 211.719 88.2598 222.157 97.0516 230.948C105.843 239.74 116.281 246.714 127.768 251.472C139.255 256.23 151.567 258.679 164 258.679C176.433 258.679 188.745 256.23 200.232 251.472C211.719 246.714 222.157 239.74 230.948 230.948C239.74 222.157 246.714 211.719 251.472 200.232C256.23 188.745 258.679 176.433 258.679 164H304Z" fill="white"/>
-                  <path d="M26.3095 138.674C32.1794 106.761 48.9523 77.8783 73.7605 56.9633C98.5688 36.0482 129.872 24.4 162.318 24.0102C194.764 23.6204 226.338 34.5131 251.641 54.8261C276.945 75.139 294.407 103.611 301.042 135.373L256.679 144.64C252.192 123.16 240.382 103.905 223.27 90.1678C206.158 76.4305 184.805 69.0639 162.862 69.3276C140.92 69.5912 119.75 77.4687 102.973 91.6131C86.1953 105.758 74.8521 125.291 70.8825 146.873L26.3095 138.674Z" fill="white"/>
-                  </svg>
-
-
-
-                </div> : null
-              }
+      {/* <Cursor/> */}
               <div className="loader" ref={el => loader2 = el}>
                 <div className="split">
                   <h1 ref={el => animname = el } style={{color: 'white'}}>
---
+++ src/components/cursor.jsx
@@ -0,0 +1,21 @@
+import React, {/*  useEffect, useRef  */} from "react"
+import "../App.css"
+const Cursor = () => {
+    const cursorRef = React.useRef(null)
+  
+    React.useEffect(() => {
+        window.addEventListener('mousemove', (event) => {
+            const { clientX, clientY } =  event
+            const mouseX = clientX - cursorRef.current.clientWidth / 2
+            const mouseY = clientY - cursorRef.current.clientHeight / 2
+            cursorRef.current.style.transform = `translate3d(${mouseX})px, ${mouseY}px, 0`
+            
+        })
+    })
+  
+    return (
+    <div className="cursor" ref={cursorRef}></div>
+    )
+}
+
+export default Cursor
