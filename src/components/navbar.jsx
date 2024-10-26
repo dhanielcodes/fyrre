35fbe5080a0c5a1a0f6b94cb276a62c6a388e74e
Daniel Adekoya <adekoyadaniel53@gmail.com>
2024-10-26 17:00:38 +0100

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,55 +1,51 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { insta2, twitter2, youtube2, wifi2, line} from "../images/index"
+import { useEffect, useState } from "react";
+import { insta2, twitter2, youtube2, wifi2, line } from "../images/index";
 
-const Navbar = ({routerLink}) => {
+const Navbar = ({ routerLink }) => {
+  return (
+    <nav data-scroll-section>
+      <div className="name">
+        <div onClick={() => routerLink("/")} className="logo">
+          FYRRE MAGAZINE
+        </div>
 
-    const location = useLocation()
-
-    let [other, setOther] = useState(true)
-
-    useEffect(() => {
-        
-        if(location.pathname === "/"){
-            setOther(false)
-
-        }else{
-            setOther(true)
-        }
-    }, [location.pathname]);
-
-    return (
-        <nav data-scroll-section>
-            <div className="name">
-                <div onClick={() => routerLink("/")} className='logo'>
-                    FYRRE MAGAZINE
-                </div>
-                {
-                    other ? (
-
-                        <div className='nav_other'>
-                            <div onClick={() => routerLink("/magazine")}>Magazine</div>
-                            <div onClick={() => routerLink("/authors")}>Authors</div>
-                            <div onClick={() => routerLink("/podcasts")}>Podcast</div>
-                            <img src={line} alt="" />
-                            <a target="_blank" rel="noreferrer" href="https://twitter.com/dhaniel0x">
-                                <img className='tweet' src={insta2} alt="" srcset="" />
-                            </a>
-                            <a target="_blank" rel="noreferrer" href="https://dribbble.com/daniel_adekoya">
-                                <img src={twitter2} alt="" srcset="" />
-                            </a>
-                            <a target="_blank" rel="noreferrer" href="https://www.figma.com/@minitechbro">
-                                <img src={youtube2} alt="" srcset="" />
-                            </a>
-                            <a target="_blank" rel="noreferrer" href="https://github.com/dhanielcodes">
-                                <img src={wifi2} alt="" srcset="" />
-                            </a>
-                        </div>
-                    ) : null
-                }
-            </div>
-        </nav>
-    );
-}
+        <div className="nav_other">
+          <div onClick={() => routerLink("/magazine")}>Magazine</div>
+          <div onClick={() => routerLink("/authors")}>Authors</div>
+          <div onClick={() => routerLink("/podcasts")}>Podcast</div>
+          <img src={line} alt="" />
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://twitter.com/dhaniel0x"
+          >
+            <img src={twitter2} alt="" srcset="" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://dribbble.com/daniel_adekoya"
+          >
+            <img className="tweet" src={insta2} alt="" srcset="" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.figma.com/@minitechbro"
+          >
+            <img src={youtube2} alt="" srcset="" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/dhanielcodes"
+          >
+            <img src={wifi2} alt="" srcset="" />
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
